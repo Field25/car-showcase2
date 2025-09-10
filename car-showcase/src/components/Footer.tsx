69863112b1f6201afa5901389095c13c18f25a49
Field25 <apichayaseskuntot@gmail.com>
2025-09-10 10:19:18 +0700

    2

--- car-showcase/src/components/Footer.tsx
+++ car-showcase/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = ({ setCurrentPage }: FooterProps) => {
   };
 
   return (
-  <footer className="bg-black text-white py-8 md:py-12">
+  <footer className="bg-black text-white py-8 md:py-12 mt-5">
     <div className="container mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
